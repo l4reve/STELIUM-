6016aeb2fda455b23c629c1517039e25ad9d26a2
l4reve <143638421+l4reve@users.noreply.github.com>
2024-05-13 17:02:29 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,11 @@
 #include <sstream>
 #include <string>
 #include <cstring>
-#include "Button.hpp"
 #include <SDL_mixer.h>
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_ttf.h>
-#include "Button.hpp"
+#include <ctime>
 
 
 using namespace std;
@@ -24,6 +23,7 @@ const int BUTTON_SPRITE_TOTAL = 3;
 const int SCREEN_WIDTH = 400;
 const int SCREEN_HEIGHT = 400;
 LButton gButtons[ROW_SIZE][COLUMN_SIZE];
+#pragma once
 
 //Tile's constants
 const int TILE_SIZE = 32;
@@ -189,7 +189,150 @@ int LTexture::getHeight() {
     return mHeight;
 }
 
+void reveal(int i, int j);
 
+class LButton
+{
+public:
+    LButton();
+    //Sets top left position
+    void setPosition(int x, int y);
+    void handleEvent(SDL_Event* e);
+    void render(int i, int j);
+
+private:
+    SDL_Point mPosition;
+};
+
+void reveal(int i, int j)
+{
+    if (sBoard[i][j] == 10 || sBoard[i][j] == 11)
+    {
+        if (sBoard[i][j] == 11)
+        {
+            countMineLeft++;
+        }
+        sBoard[i][j] = board[i][j];
+        countTileLeft--;
+        if (sBoard[i][j] == 0)
+        {
+            if (i < ROW_SIZE - 1)
+                reveal(i + 1, j);
+            if (i > 0)
+                reveal(i - 1, j);
+            if (j < COLUMN_SIZE - 1)
+                reveal(i, j + 1);
+            if (j > 0)
+                reveal(i, j - 1);
+            if (i > 0 && j > 0)
+                reveal(i - 1, j - 1);
+            if (i < ROW_SIZE - 1 && j < COLUMN_SIZE - 1)
+                reveal(i + 1, j + 1);
+            if (i > 0 && j < COLUMN_SIZE - 1)
+                reveal(i - 1, j + 1);
+            if (i < ROW_SIZE - 1 && j > 0)
+                reveal(i + 1, j - 1);
+        }
+    }
+}
+
+LButton::LButton()
+{
+    mPosition.x = 0;
+    mPosition.y = 0;
+}
+
+void LButton::setPosition(int x, int y)
+{
+    mPosition.x = x;
+    mPosition.y = y;
+}
+
+void LButton::handleEvent(SDL_Event* e)
+{
+    //If mouse event happened
+    if (e->type == SDL_MOUSEMOTION || e->type == SDL_MOUSEBUTTONDOWN || e->type == SDL_MOUSEBUTTONUP)
+    {
+        //Get mouse position
+        int x, y;
+        SDL_GetMouseState(&x, &y);
+
+        int i = (y - DISTANCE_BETWEEN) / TILE_SIZE;
+        int j = (x - DISTANCE_BETWEEN) / TILE_SIZE;
+
+        //Check if mouse is in button
+        bool inside = true;
+
+        //Mouse is left of the button
+        if (x < mPosition.x)
+        {
+            inside = false;
+        }
+        //Mouse is right of the button
+        else if (x > mPosition.x + TILE_SIZE)
+        {
+            inside = false;
+        }
+        //Mouse above the button
+        else if (y < mPosition.y)
+        {
+            inside = false;
+        }
+        //Mouse below the button
+        else if (y > mPosition.y + TILE_SIZE)
+        {
+            inside = false;
+        }
+
+        //Mouse is inside button
+        if (inside)
+        {
+            if (e->type == SDL_MOUSEBUTTONDOWN)
+            {
+                //Play the sound effect
+                Mix_PlayChannel(-1, click, 0);
+
+                //Set mouse clicked
+                switch (e->button.button)
+                {
+                case SDL_BUTTON_LEFT:
+                {
+                    reveal(i, j);
+                    if (board[i][j] == 9)
+                    {
+                        gameOver = true;
+                    }
+                    break;
+                }
+                case SDL_BUTTON_RIGHT:
+                {
+                    if (sBoard[i][j] >= 10)
+                    {
+                        if (sBoard[i][j] == 10)
+                        {
+                            if (countMineLeft == 0) break;
+                            sBoard[i][j] = 11;
+                            countMineLeft--;
+                        }
+                        else
+                        {
+                            sBoard[i][j] = 10;
+                            countMineLeft++;
+                        }
+                    }
+                    break;
+                }
+                }
+            }
+        }
+    }
+}
+
+void LButton::render(int i, int j)
+{
+    //Show current button sprite
+    gButtonSpriteSheetTexture.render(mPosition.x, mPosition.y, &gSpriteClips[sBoard[i][j]]);
+}
 int main(int argc, char* args[])
 {
     if (!init())
