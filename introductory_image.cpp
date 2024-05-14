ad767a12676099ca1b3e5b8e4474400217e4ec71
l4reve <143638421+l4reve@users.noreply.github.com>
2024-05-14 17:59:47 +0700

    Update

--- introductory_image.cpp
+++ introductory_image.cpp
@@ -1,4 +1,3 @@
-// introductory_image.cpp
 #include "introductory_image.hpp"
 
 IntroductoryImage::IntroductoryImage() {}
@@ -6,11 +5,9 @@ IntroductoryImage::IntroductoryImage() {}
 IntroductoryImage::~IntroductoryImage() {}
 
 bool IntroductoryImage::loadIntroductoryImage() {
-    // Load the introductory image
     return mIntroTexture.loadFromFile("background1.png");
 }
 
 void IntroductoryImage::render() {
-    // Render the introductory image
-    mIntroTexture.render(0, 0); // Adjust position as needed
+    mIntroTexture.render(0, 0); 
 }
