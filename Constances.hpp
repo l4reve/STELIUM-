7d098551e1b9c3d2e692227b267a7385616165d5
l4reve <143638421+l4reve@users.noreply.github.com>
2024-05-14 17:51:10 +0700

    ADD

--- Constances.hpp
+++ Constances.hpp
@@ -1,10 +1,17 @@
 #pragma once
 
+//Tile's constants
 const int TILE_SIZE = 32;
 const int BUTTON_SPRITE_TOTAL = 12;
-const int MINE_COUNT = 12;
-const int ROW_SIZE = 10;
-const int COLUMN_SIZE = 10;
-const int SCREEN_WIDTH = 460;
-const int SCREEN_HEIGHT = 460;
+
+//Number of mines
+const int MINE_COUNT = 18;
+
+//Table's size
+const int ROW_SIZE = 12;
+const int COLUMN_SIZE = 12;
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 572;
+const int SCREEN_HEIGHT = 572;
 const int DISTANCE_BETWEEN = (SCREEN_WIDTH - ROW_SIZE * TILE_SIZE) / 2;
